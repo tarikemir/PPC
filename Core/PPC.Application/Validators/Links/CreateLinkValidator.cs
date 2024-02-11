9fc1291440bb6438aabe6ddb39022e126899d9e9
tarikemir <81103446+tarikemir@users.noreply.github.com>
2024-02-11 16:04:42 +0300

    Fluent Validation is added & ViewModel for validation created.

---
+++ Core/PPC.Application/Validators/Links/CreateLinkValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using PPC.Application.ViewModels.Links;
+
+namespace PPC.Application.Validators.Links
+{
+    public class CreateLinkValidator : AbstractValidator<VM_Create_Link>
+    {
+        public CreateLinkValidator()
+        {
+            RuleFor(c => c.OriginalUrl)
+                .NotEmpty()
+                .WithMessage("Can't accept empty link!")
+                .MinimumLength(3)
+                .WithMessage("Please enter a value that is greater than 3 char!");
+        }
+    }
+}
---
+++ Core/PPC.Application/ViewModels/Links/VM_Create_Link.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PPC.Application.ViewModels.Links
+{
+    public class VM_Create_Link
+    {
+        public string OriginalUrl { get; set; }
+        public string ShortenedUrl { get; set; }
+        public long ClickCount { get; set; }
+    }
+}
